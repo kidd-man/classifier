5c6001d0bc11ce4ceba0eb031ecf2e2c6a74444c
kidd-man <ta1ku2ra3ma4ka5n@gmail.com>
2018-06-14 18:38:39 +0900

    almost finished

--- definitions.py
+++ definitions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rd
 
 
 def levenshtein_distance(str1: str, str2: str):
@@ -17,7 +18,98 @@ def levenshtein_distance(str1: str, str2: str):
             arr[i+1, j+1] = min(arr[i, j+1] + 1,
                                 arr[i+1, j] + 1,
                                 arr[i, j] + cost)
+    return arr[n][m]
 
+
+def apply_ldist_to_datas(dlist: np.array):
+    n = len(dlist)
+    arr = np.zeros((n, n))
+    arr[:, :] = np.nan
+
+    for i in range(n):
+        for j in range(i+1, n):
+            arr[i, j] = levenshtein_distance(dlist[i], dlist[j])
     return arr
 
 
+def kmeans(arr: np.array, cnum: int):
+    tags = np.zeros(arr.shape)
+    n = tags.shape[0]
+    tags[:] = np.nan
+    mlist = np.array(list(range(cnum)), dtype='float')
+
+    for i in range(n):
+        for j in range(i+1, n):
+            tags[i, j] = rd.randrange(cnum)
+
+    def itr():
+        # 中心の更新
+        for i in range(cnum):
+            index = np.where(tags == i)
+            s = 0
+            for j, k in zip(index[0], index[1]):
+                s += arr[j][k]
+            mlist[i] = float(s) / len(index[0])
+        # 新しいタグ付け
+        for i in range(n):
+            for j in range(i+1, n):
+                idx = np.abs(mlist - arr[i, j]).argmin()
+                tags[i, j] = idx
+    while True:
+        print(mlist)
+        lasttags = tags
+        itr()
+        if np.all((lasttags == tags) | np.isnan(lasttags) & np.isnan(tags)):
+            break
+    print(mlist)
+    return tags, mlist
+
+
+def outdotfile(fname:str, arr: np.array, cutoff: int, datas: np.array):
+    f = open(fname + '.dot', 'w')
+    nodes = set([])
+    edges = set([])
+    nums = 0
+    mindist = np.nanmin(arr)
+    maxdist = np.nanmax(arr)
+    for i in range(int(mindist), int(maxdist) + 1):
+        idx = np.where(arr == i)
+        nums += len(idx[0])
+        for j, k in zip(idx[0], idx[1]):
+            nodes.add(j)
+            nodes.add(k)
+            edges.add((j, k))
+        if len(nodes) >= cutoff:
+            break
+    codes = "digraph graph1{\n" \
+            "graph[layout=neato];\n" \
+            "edge[arrowhead=none,len=2.6, penwidth=2];\n" \
+            'node[shape=circle,fixedsize="MS Gothic",penwidth=2];\n'
+    for i in nodes:
+        codes += 'a' + str(i) + '[label="' + str(i+1) + '"];\n'
+
+    for t in edges:
+        codes += 'a' + str(t[0]) + '->' + 'a' + str(t[1]) + ';\n'
+
+    codes += '}'
+
+    f.write(codes)
+    f.close()
+
+
+def makegraph(arr: np.array, threshold: int):
+    nodes = set([])
+    edges = set([])
+    shape = arr.shape()
+    for i in range(0, shape[0]+1):
+        for j in range(i+1, shape[1]+1):
+            if arr[i, j] <= threshold:
+                nodes.add(i)
+                nodes.add(j)
+                edges.add((i, j))
+    return nodes, edges
+
+def clusteringcoefficient(graph: tuple):
+    clist=[]
+    nodes = graph[0]
+    edges = graph[1]
--- run.py
+++ run.py
@@ -1,7 +1,74 @@
-import definitions as ld
-import csv
+import definitions as df
+import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram
 
 csv_read = pd.read_csv("./Ogura_Hyakunin_Isshu.csv-master/ogurahyakuninisshu.csv", index_col=0, usecols=[0, 4, 5])
 
-print(csv_read)
+dataset = np.sum(csv_read.values, axis=1)
+#dataset = ['うらみわびほさぬそでだにあるものをこひにくちなむなこそをしけれ',
+#           'おもひわびさてもいのちはあるものをうきにたへぬはなみだなりけり',
+#           'はるのよのゆめばかりなるたまくらにかひなくたたむなこそをしけれ',
+#           'きみがためはるののにいでてわかなつむわがころもでにゆきはふりつつ',
+#           'きみがためをしからざりしいのちさへながくもがなとおもひけるかな']
+for i in [31, 32, 33]:
+    summation = 0
+    for j in dataset:
+        if len(j) == i:
+            summation += 1
+    print(str(i) + '文字: ' + str(summation) + '首')
+
+arr = df.apply_ldist_to_datas(dataset)
+mindist = np.nanmin(arr)
+maxdist = np.nanmax(arr)
+df.outdotfile('graph', arr, 60, dataset)
+plt.hist(arr[~np.isnan(arr)], rwidth=0.8, ec='black', bins=range(int(mindist), int(maxdist)+2))
+plt.title('histgram')
+plt.xlabel('Levenshtein Distance')
+plt.ylabel('numbers of Waka pair')
+plt.savefig('hist.png')
+
+for i in range(int(mindist), int(maxdist)+1):
+    idx = np.where(arr == i)
+    print('編集距離' + str(i) + ':' + str(len(idx[0])) + '個')
+
+print('総数が11個以下の編集距離数を取り上げて表示します.')
+for i in range(int(mindist), int(maxdist)+1):
+    idx = np.where(arr == i)
+    if len(idx[0]) <= 11:
+        print('編集距離:' + str(i))
+        for j, k in zip(idx[0], idx[1]):
+            print('"' + dataset[j] + '"---"' + dataset[k] + '"')
+
+np.nan_to_num(arr)
+mdist = np.sum(np.nan_to_num(arr) + np.nan_to_num(arr.T), axis=0)/99
+mminidx = np.argmin(mdist)
+mmaxidx = np.argmax(mdist)
+
+print('最小平均距離(' + str(mdist[mminidx]) + '):"' + dataset[mminidx] + '"')
+print('最大平均距離(' + str(mdist[mmaxidx]) + '):"' + dataset[mmaxidx] + '"')
+
+print('平均距離ランキング')
+rank = np.argsort(mdist)
+for i in range(100):
+    print(str(i+1) + '位(' + str(mdist[rank[i]]) + '):"' + dataset[rank[i]] + '"')
+# res = df.kmeans(arr, 5)
+# print(res[0])
+
+"""
+minidx = np.where(res[0] == np.argmin(res[1]))
+for i, j in zip(minidx[0], minidx[1]):
+    print((i, j, arr[i, j]))
+print('*************')
+maxidx = np.where(res[0] == np.argmax(res[1]))
+for i, j in zip(maxidx[0], maxidx[1]):
+    print((i, j, arr[i, j]))
+"""
+# Z = linkage(arr, 'ward')
+# dn = dendrogram(Z)
+# plt.show()
+# print(np.where(arr == np.nanmin(arr)))
+#linkage(ld.levenshtein_distance(csv.read))
